2780736b3afebd18c17d29a533d02547c38b7d94
Juan Camilo Hernández Ramírez <Juancazzhr@gmail.com>
2023-11-28 20:32:26 -0500

    feat(project): add package template
    Include all items for Unity package
    - Directories
    - Assemblies
    - Suggested elements in Unity Docs

---
+++ XSO/Assets/Runtime/Variables/BaseVar.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Juancazzhr.Xso.Runtime.Variables
+{
+    /// <summary>
+    /// Base class for variables with a value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BaseVar<T> : ScriptableObject
+    {
+        /// <summary>
+        /// Create an instance of the variable. Creates an asset.
+        /// </summary>
+        /// <returns></returns>
+        public static BaseVar<T> CreateInstance() => CreateInstance<BaseVar<T>>();
+
+        /// <summary>
+        /// The value of the variable.
+        /// </summary>
+        [Tooltip("The value of the variable.")]
+        [SerializeField] protected T value;
+
+        /// <summary>
+        /// Event invoked when the value of the variable changes.
+        /// </summary>
+        [Tooltip("Event invoked when the value of the variable changes.")]
+        [SerializeField] protected UnityEvent<T> onValueChanged = new();
+
+        /// <summary>
+        /// Get or set the value of the variable.
+        /// </summary>
+        public T Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                onValueChanged?.Invoke(value);
+            }
+        }
+
+        /// <summary>
+        /// Get the event invoked when the value of the variable changes.
+        /// </summary>
+        public event UnityAction<T> OnValueChanged
+        {
+            add => onValueChanged.AddListener(value);
+            remove => onValueChanged.RemoveListener(value);
+        }
+    }
+}
---
+++ XSO/Assets/Tests/Runtime/RuntimeExampleTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Juancazzhr.Xso.Tests.Runtime
+{
+    public class RuntimeExampleTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void RuntimeExampleTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator RuntimeExampleTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
